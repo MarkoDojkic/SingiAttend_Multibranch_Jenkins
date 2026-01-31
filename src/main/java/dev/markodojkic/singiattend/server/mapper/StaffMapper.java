7cd0e6252136c3df1c59eab5c65711acede090bf
Марко Дојкић <marko.dojkic@gmail.com>
2026-01-31 18:04:41 +0100

    Corrected controller paths and added configurable context path and cors origins; Updated dependencies to latest

--- src/main/java/dev/markodojkic/singiattend/server/mapper/StaffMapper.java
+++ src/main/java/dev/markodojkic/singiattend/server/mapper/StaffMapper.java
@@ -6,7 +6,6 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
