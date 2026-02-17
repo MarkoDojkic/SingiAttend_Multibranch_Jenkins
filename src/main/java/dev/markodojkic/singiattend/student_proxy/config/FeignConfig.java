8fe3fb96d84b63ecbeadce30b5b114ecdaadf720
Марко Дојкић <marko.dojkic@gmail.com>
2026-02-17 19:16:04 +0100

    fix(ISSUE-1): Corrected configuration to separate FeignClient connection with and without kubernetes cluster; Added truststore for https connection; Added

--- src/main/java/dev/markodojkic/singiattend/student_proxy/config/FeignConfig.java
+++ src/main/java/dev/markodojkic/singiattend/student_proxy/config/FeignConfig.java
@@ -1,13 +1,28 @@
 package dev.markodojkic.singiattend.student_proxy.config;
 
+import feign.Client;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
 import feign.RequestInterceptor;
+import org.springframework.core.io.Resource;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.net.ssl.*;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.SecureRandom;
+
 @Configuration
 public class FeignConfig {
+
+    @Value("${server.ssl.trust-store}")
+    private Resource trustStoreResource;
+
+    @Value("${server.ssl.trust-store-password}")
+    private String trustStorePassword;
+
     @Bean
     public RequestInterceptor forwardHeaders() {
         return requestTemplate -> {
@@ -24,5 +39,24 @@ public class FeignConfig {
                 if (authorization != null) requestTemplate.header("Authorization", authorization);
             }
         };
+    }
+
+    @Bean
+    public Client feignClient() throws Exception {
+        KeyStore trustStore = KeyStore.getInstance("JKS");
+        try (InputStream is = trustStoreResource.getInputStream()) {
+            trustStore.load(is, trustStorePassword.toCharArray());
+        }
+
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        tmf.init(trustStore);
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, tmf.getTrustManagers(), new SecureRandom());
+
+        return new Client.Default(
+                sslContext.getSocketFactory(),
+                HttpsURLConnection.getDefaultHostnameVerifier()
+        );
     }
 }
